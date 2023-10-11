2f6678c62b65c2bfc0ca570a433b05dca67d9a34
iquelli <raquelbb28.08@gmail.com>
2023-10-11 13:41:28 +0100

    Add 1 test for L2-2W

---
+++ lab2/cache_simulator/tests/Test4_L2_2W.c
@@ -0,0 +1,30 @@
+#include "../src/3/L2Cache_2W.h"
+#include <stdint.h>
+
+int main() {
+    uint32_t clock;
+    int value;
+
+    resetTime();
+    initCaches();
+
+    //chech if write/read adress match
+    for (int i = 4; i < 257; i = i*4) {
+        write(i, (unsigned char *)(&i));
+        clock = getTime();
+        printf("Write; Address %d; Value %d; Time %d\n", i, i, clock);
+    }
+
+    // writes again to see if it's going to update
+    int i = 2;                       
+    write(16,(unsigned char *)(&i));    
+
+
+    for (int i = 256; i > 1; i = i / 4) {
+        read(i, (unsigned char *)(&value));
+        clock = getTime();
+        printf("Read; Address %d; Value %d; Time %d\n", i, value, clock);
+    }
+
+    return 0;
+}
